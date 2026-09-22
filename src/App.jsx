1c90dac9d3ea1ea5cce32a0ccaddee6264f9006b
lohithakshith <lohithakshith2007@gmail.com>
2026-09-22 22:22:09 +0530

    chore: Create job tracker application structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Job Application Tracker</h1>
+      <p>Welcome! Your tracker will be built here.</p>
+    </div>
+  )
+}
+
+export default App
